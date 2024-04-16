8ef10f0826d05dad3476bc8f47738bef558e8127
aldanielago <aldanielago@gmail.com>
2024-04-15 21:40:58 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './index.css'
+
+const board = Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0))
+
+function App() {
+  console.log(board)
+  return (
+    <>
+      <div className="board">
+        { board.map((row, i) => (
+          <div key={i} className="row">
+            { row.map((cell, j) => (
+              <div key={j} className="cell">{cell}</div>
+            )) }
+          </div>
+        )) }
+      </div>
+    </>
+  )
+}
+
+export default App
